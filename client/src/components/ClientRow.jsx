c8beb0499a9e1af120a43eae07bc516fa602f50b
Mikhel Patel <mikhel@zeroomega.com>
2023-06-27 08:15:11 -0400

    deleteClient

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,13 +1,20 @@
 import "../App.css";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/clientMutations";
 
 export default function ClientRow({ client }) {
+
+const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id }
+});
+
   return (
     <tr className="table--row">
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button>Delete</button>
+        <button onClick={deleteClient}>Delete</button>
       </td>
     </tr>
   );
